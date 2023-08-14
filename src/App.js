e130bd12004c95c5e925e9d10972f5dbc46eb887
Saurav <phatesaurav@gmail.com>
2023-08-14 23:58:50 +0530

    first commit by saurav

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/components/navigation/Navigation';
+
+function App() {
+  return (
+    <div className="">
+      <Navigation/>
+      <div>
+        home
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
